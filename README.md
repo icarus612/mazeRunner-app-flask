2edf3be99f1b7250a964f23158fd1dcb6179e04f
icarus <ellishogan95@gmail.com>
2019-11-15 22:24:17 -0500

    all

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Maze Runner
+
+## An Original Design By: Dev.Icarus
+
+#### My github: https://github.com/icarus612
+#### Project github: https://github.com/icarus612/mazeRunner-PY
+
+#### A Python Algorithms Project
+
+Feel free to use it and change it how you like - Maze runner operates under MIT licenses.
+
+### Project Description:
+A maze runner project with a Node, Maze, and Runner class, as well as a Solver script. Maze class stores maze start, end, wall, and floor spaces, and runner class evaluates free spaces and builds node paths between to solve it using a breadth first search. **The solver file can take in any type of maze.** As long as the maze has a uniqe start and end character, and is wrapped in wall characters the solver function can evaluate it as a maze an 'run' it. The solver script can take between 1 - 3 arguments. The first argument(s) is either a maze file to solve or a set of height and width, and  the last argument is an optional save file (default "completed.txt").
+
+### How To Run:
+``` python solver.py```
+It guides you throught the rest!
---
+++ main.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, url_for, session, request, redirect
+import requests
+import os
+from maze import Maze
+from runner import Runner
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/<maze>', methods=['GET', 'POST'])
+def index(maze=None):
+	print(session.get(maze))
+	return render_template('index.html', maze=session[maze])
+
+@app.route('/download/<file_name>')
+def download(city, country, state=None):
+	pass
+
+@app.route('/solve_maze', methods=['POST'])
+def solve_maze():
+	try:
+		city = request.form['city']
+		state = request.form['state'] if request.form['state'] != "" else None
+		country = request.form['country']
+		return redirect(url_for('weather', city=city, country=country, state=state))
+	except:
+		return redirect(url_for('index'))
+
+@app.route('/make_maze', methods=['POST'])
+def make_maze():
+	height = int(request.form['height'])
+	width = int(request.form['width'])
+	maze_type = request.form['type']
+	maze = Maze(build=[(width, height), maze_type])
+	maze.view_layout()
+	runner = Runner(maze)
+	runner.make_node_paths()
+	complete = "Yes" if runner.completed else "No"
+	print(f"Is maze possible? {complete}")
+	session[maze] = maze
+	if runner.completed:	
+		runner.build_path()
+	runner.view_completed()
+	return redirect(url_for('index', maze=maze))
+
+
+
+def open_and_build(file):
+	with open(file) as m:
+		txt = m.read()
+		m1 = [list(i) for i in txt.split("\n")]
+		flat = [y for x in m1 for y in x]
+		s = set(txt)
+		wall = txt[0][0]
+		s.remove("\n")
+		s.remove(wall)
+		for i in s:
+			if flat.count(i) == 1:
+				pass
+			else: 
+				space = i
+		s.remove(space)
+		flat[:] = [x for x in flat if x != wall and x != space]
+		start = flat[0]
+		end = flat[1]
+		return Maze(m1, start, end, wall, space)
+
+
+
+
+port = int(os.environ.get('PORT', 5000)) 
+if __name__ == '__main__':
+	app.run(debug=True, port=port)
