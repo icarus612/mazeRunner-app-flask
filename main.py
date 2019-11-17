e4a9a6677dce918418b52adb77e2fc5b133339e3
icarus <ellishogan95@gmail.com>
2019-11-17 14:26:09 -0500

    update

--- main.py
+++ main.py
@@ -6,21 +6,8 @@ from runner import Runner
 
 app = Flask(__name__)
 app.secret_key = "ai2927"
-
-@app.route('/')
-@app.route('/built')
-def index():
-	layout = session.get(request.args.get('maze'))
-	solved = session.get(request.args.get('solved'))
-	print(layout, request.args.get('maze'))
-	return render_template('index.html', layout=layout, solved=solved)
-
-@app.route('/make_maze', methods=['POST'])
-def make_maze():
-	height = int(request.form['height'])
-	width = int(request.form['width'])
-	maze_type = request.form['type']
-	maze = Maze(build=[(width, height), maze_type])
+		
+def run_maze(maze):
 	runner = Runner(maze)
 	runner.make_node_paths()
 	session[f"{maze}"] = maze.view_layout()
@@ -29,6 +16,45 @@ def make_maze():
 	if runner.completed:	
 		runner.build_path()
 	session[f"{runner}"] = runner.view_completed()
+	return runner
+
+@app.route('/')
+@app.route('/built')
+def index():
+	layout = session.get(request.args.get('maze'))
+	solved = session.get(request.args.get('solved'))
+	return render_template('index.html', layout=layout, solved=solved)
+
+@app.route('/upload_maze', methods=['POST'])
+def upload_maze():
+	maze_file = request.files['maze_file']
+	txt = maze_file.stream.read().decode("utf-8")
+	m1 = [list(i) for i in txt.split("\n")]
+	flat = [y for x in m1 for y in x]
+	s = set(txt)
+	wall = txt[0][0]
+	s.remove("\n")
+	s.remove(wall)
+	for i in s:
+		if flat.count(i) == 1:
+			pass
+		else: 
+			space = i
+	s.remove(space)
+	flat[:] = [x for x in flat if x != wall and x != space]
+	start = flat[0]
+	end = flat[1]
+	maze = Maze(m1, start, end, wall, space)
+	runner = run_maze(maze)
+	return redirect(url_for('index', maze=maze, solved=runner))
+
+@app.route('/make_maze', methods=['POST'])
+def make_maze():
+	height = int(request.form['height'])
+	width = int(request.form['width'])
+	maze_type = request.form['type']
+	maze = Maze(build=[(width, height), maze_type])
+	runner = run_maze(maze)
 	return redirect(url_for('index', maze=maze, solved=runner))
 
 
