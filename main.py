afed81c72b0af4e006de7d9348e59824c8e61cc4
icarus <ellishogan95@gmail.com>
2019-11-17 15:55:26 -0500

    update

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ def index():
 @app.route('/upload_maze', methods=['POST'])
 def upload_maze():
 	maze_file = request.files['maze_file']
-	print(maze_file)
+	if maze_file == '':
+		return redirect(url_for('index', maze=None, solved=None))
 	txt = maze_file.stream.read().decode("utf-8")
 	m1 = [list(i) for i in txt.split("\n")]
 	flat = [y for x in m1 for y in x]
